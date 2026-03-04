c1ad9b21c10a1d310ee4a3c97bf63908d9e79e46
Peddi Yeswanth <2400031534@kluniversity.in>
2026-03-04 12:04:43 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-public static void main(String[] args){
-System.out.println("hello world!!");
-System.out.println("Bug fixed version");
-System.out.println("Hello Git - Feature and Bug Fix applied");
-}
-}
